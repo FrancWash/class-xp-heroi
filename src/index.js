4e9b33ba4b3558ceae4d4184ddd215cde28e8738
FrancWash <fvilela216@gmail.com>
2024-03-10 09:09:14 -0300

    Indentificando o nosso herói.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let nomeDoHeroi = "Homem de Férro"
+let xpDoHeroi = 5.548
+
+console.log("Olá, eu sou o: " + nomeDoHeroi + " 🍩")
+console.log("E meu poder é superior a esses monstros que querem invadir a terra " + xpDoHeroi + " 💪")
